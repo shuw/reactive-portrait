789ad7ccce908474745968e2db3a248dc14f6723
Shu Wu <shuwu83@gmail.com>
2020-10-19 22:27:10 -0700

    Updates

---
+++ precache-manifest.5f9561b07a443a9a2ab92c3c0ecdf90b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1093e2687864d7a563fa2a2c38611d",
+    "url": "/reactive-portrait/index.html"
+  },
+  {
+    "revision": "94fb8120c20c1a4237a0",
+    "url": "/reactive-portrait/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fe940f919b74fc801bd",
+    "url": "/reactive-portrait/static/js/2.4ebcaa4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/reactive-portrait/static/js/2.4ebcaa4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94fb8120c20c1a4237a0",
+    "url": "/reactive-portrait/static/js/main.95cee661.chunk.js"
+  },
+  {
+    "revision": "6c3856abd7465747d631",
+    "url": "/reactive-portrait/static/js/runtime-main.64dd17f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactive-portrait/static/media/logo.5d5d9eef.svg"
+  }
+]);
