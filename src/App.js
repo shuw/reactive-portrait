5990d152a02be81ff74971745b0dface2e57fd7d
Shu Wu <shuwu83@gmail.com>
2020-10-30 08:35:42 -0700

    support multiple portraits

--- src/App.js
+++ src/App.js
@@ -17,18 +17,30 @@ export default class App extends React.Component {
 
   render() {
     const params = new URL(document.location).searchParams;
-    const name = params.get("name") ?? "shuw";
-    const hideStateInfo = params.get("hideStateInfo") !== null;
+    const names = (params.get("names") ?? params.get("name") ?? "shuw").split(
+      ","
+    );
 
-    return (
-      <div className="App">
-        <header className="App-header">
+    const portraits = names.map((name) => {
+      return (
+        <div style={{ display: "inline-block", padding: "10px" }}>
           <ReactivePortrait
             snippetsMediaPath={"/reactive-portrait/media/" + name}
             onSnippetChanged={this.onPortraitChanged}
             width={400}
             height={400}
           />
+        </div>
+      );
+    });
+
+    const hideStateInfo =
+      params.get("hideStateInfo") !== null || portraits.length !== 1;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>{portraits}</div>
           {hideStateInfo ? null : (
             <div style={{ paddingTop: "20px" }}>{this.state.snippetName}</div>
           )}
